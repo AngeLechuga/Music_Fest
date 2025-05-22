4ed4e31e84a834461a02012c21dc85bc72860951
Angel Lechuga Milpas <angellechugamilpas@MacBook-Air-de-Angel.local>
2025-05-22 13:58:02 -0600

    Compiling sass from Gulp and creating build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-export function hola( done ) {
+// -- extraer sass desde el gulp
+import { src, dest } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass);
+
+
+// -- compilar Sass
+export function css(done) {
+    // -- ubicanco archivo
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('build/css'))
+    
+    done();
+}
+/* export function hola( done ) {
     console.log('Hola desde gulpfile.js');
 
     done();
-};
+}; */
